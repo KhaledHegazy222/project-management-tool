8d9f85d1f0a7e832b02ed5f73c5124fab717caa7
Khaled Hegazy <khaledhegazy870@gmail.com>
2023-06-18 14:16:40 +0300

    Configure project invitations form

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,8 @@ import "@fontsource/inter";
 import { routes } from "./pages/routes";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import theme from "@/theme";
-import 'react-toastify/dist/ReactToastify.css';
+import "react-toastify/dist/ReactToastify.css";
+import "react-multi-email/dist/style.css";
 import { ToastContainer } from "react-toastify";
 
 function App() {
--- client/src/components/Dashboard/Members.tsx
+++ client/src/components/Dashboard/Members.tsx
@@ -1,18 +1,63 @@
-import React, { useEffect, useState } from "react";
-import { Box, Button, Grid, Paper, Typography } from "@mui/material";
-import { Add } from "@mui/icons-material";
+import React, {
+  FormEvent,
+  FormEventHandler,
+  useCallback,
+  useEffect,
+  useState,
+} from "react";
+import {
+  Box,
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  Grid,
+  IconButton,
+  Paper,
+  Typography,
+} from "@mui/material";
+import { Add, Close } from "@mui/icons-material";
 import MemberCard, { memberType } from "./MemberCard";
 import { useAuth } from "@/contexts/AuthContext";
 import { axiosServer } from "@/services";
 import { useParams } from "react-router-dom";
 import { AxiosError } from "axios";
+import { ReactMultiEmail } from "react-multi-email";
+import { toast } from "react-toastify";
 
 const Members = () => {
   const { id } = useParams();
-
   const { auth } = useAuth();
+  const [addMembersDialogShow, setAddMembersDialogShow] =
+    useState<boolean>(false);
+  const [addedEmails, setAddedEmails] = useState<string[]>([]);
   const [membersList, setMembersList] = useState<memberType[]>([]);
-
+  const handleSubmit = useCallback(
+    async (e: FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      if (addedEmails.length === 0) {
+        toast.error("You need to enter at least one email", {
+          autoClose: 1000,
+          position: "top-center",
+        });
+        return;
+      }
+      try {
+        const response = await axiosServer.post(
+          `/project/${id}/member`,
+          {
+            members_mail: addedEmails,
+          },
+          { headers: { Authorization: `Bearer ${auth}` } }
+        );
+        console.log(response.data);
+      } catch (error) {
+        console.log((error as AxiosError).response?.data);
+      }
+    },
+    [id, addedEmails, auth]
+  );
   useEffect(() => {
     loadData();
     async function loadData() {
@@ -41,7 +86,7 @@ const Members = () => {
         console.log((error as AxiosError).response?.data);
       }
     }
-  }, [auth]);
+  }, [auth, id]);
   return (
     <Box sx={{ padding: "0 40px" }}>
       <Typography
@@ -84,6 +129,7 @@ const Members = () => {
             }}
           >
             <Button
+              onClick={() => setAddMembersDialogShow(true)}
               sx={{
                 display: "flex",
                 alignItems: "center",
@@ -105,6 +151,81 @@ const Members = () => {
           </Paper>
         </Grid>
       </Grid>
+      <Dialog
+        open={addMembersDialogShow}
+        onClose={() => setAddMembersDialogShow(false)}
+      >
+        <form onSubmit={handleSubmit}>
+          <DialogTitle
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+            }}
+          >
+            <Typography
+              sx={{
+                fontSize: "1.5rem",
+                fontWeight: "600",
+              }}
+            >
+              Add Members
+            </Typography>
+            <IconButton onClick={() => setAddMembersDialogShow(false)}>
+              <Close />
+            </IconButton>
+          </DialogTitle>
+          <DialogContent>
+            <ReactMultiEmail
+              placeholder="Enter Invited Email Addresses"
+              emails={addedEmails}
+              style={{
+                minWidth: "300px",
+              }}
+              onChange={(emails) => {
+                setAddedEmails(emails);
+              }}
+              getLabel={(email, index, removeEmail) => {
+                return (
+                  <Box key={index} data-tag>
+                    <Typography
+                      sx={{
+                        fontSize: "0.8rem",
+                      }}
+                    >
+                      {email}
+                    </Typography>
+                    <IconButton
+                      data-tag-handle
+                      onClick={() => removeEmail(index)}
+                    >
+                      <Close
+                        sx={{
+                          fontSize: "1.1rem",
+                        }}
+                      />
+                    </IconButton>
+                  </Box>
+                );
+              }}
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button
+              variant="contained"
+              type="submit"
+              sx={{
+                textTransform: "none",
+                fontSize: "1.1rem",
+                fontWeight: "500",
+                margin: "10px",
+              }}
+            >
+              Add
+            </Button>
+          </DialogActions>
+        </form>
+      </Dialog>
     </Box>
   );
 };
