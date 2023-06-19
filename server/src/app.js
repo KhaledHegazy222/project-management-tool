c39a9c02c90ee916e2bfd8253d09e12f650e5e14
Omar622 <omaramin622@gmail.com>
2023-06-19 23:14:24 +0300

    Create socket
    * handle comment & task creations from server side

--- server/src/app.js
+++ server/src/app.js
@@ -1,13 +1,17 @@
 const cors = require('cors');
 const express = require('express');
 const bodyParser = require('body-parser');
+const http = require('http');
+const { Server } = require('socket.io');
 require('dotenv').config();
+const verifySocketHelper = require('./middlewares/verifySocketHelper');
 const userRoute = require('./route/userRoute');
 const projectRoute = require('./route/projectRoute');
 const requestRoute = require('./route/requestRoute');
 const taskRoute = require('./route/taskRoute');
 
 const app = express();
+const server = http.createServer(app);
 
 app.use(cors());
 // parse application/json parser
@@ -20,6 +24,26 @@ app.use('/api/v1/project', projectRoute);
 app.use('/api/v1/request', requestRoute);
 app.use('/api/v1/task', taskRoute);
 
-app.listen(process.env.PORT, () => {
+// socket
+const io = new Server(server);
+
+io.use(verifySocketHelper.verifySocket);
+
+io.on('connection', (socket) => {
+  console.log(`User Connected: ${socket.id}`);
+
+  socket.on('send_task_comment', (data) => {
+    const { taskId } = data;
+    console.log('send_task_comment', taskId, socket.projectId);
+    socket.to(socket.projectId).emit('receive_task_comment', { taskId });
+  });
+
+  socket.on('send_create_task', () => {
+    console.log('send_create_task', socket.projectId);
+    socket.to(socket.projectId).emit('receive_task_create', { projectId: socket.projectId });
+  });
+});
+
+server.listen(process.env.PORT, () => {
   console.log('server started...');
 });
---
+++ server/src/middlewares/verifySocketHelper.js
@@ -0,0 +1,15 @@
+/* eslint-disable consistent-return */
+const jwt = require('jsonwebtoken');
+
+exports.verifySocket = (socket, next) => {
+  const { token, projectId } = socket.handshake.query;
+
+  if (token == null) return next(new Error('Unauthorized'));
+  if (projectId == null) return next(new Error('Bad Request'));
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err) => {
+    if (err) return next(new Error('Unauthorized'));
+    socket.join(projectId);
+    next();
+  });
+};
