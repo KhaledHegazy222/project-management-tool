5ae22d68efad00b2d1fba8f5477fb1055f9cee11
Omar622 <omaramin622@gmail.com>
2023-06-20 12:32:35 +0300

    Update user schema

--- server/src/scripts/ddl.sql
+++ server/src/scripts/ddl.sql
@@ -21,7 +21,6 @@ delete from "user" where true;
 -- Create the 'user' table
 CREATE TABLE "user" (
   user_id SERIAL PRIMARY KEY,
-  creation_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   active BOOL DEFAULT 'false',
   mail VARCHAR(255) UNIQUE NOT NULL CHECK (mail ~ '[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}'),
   password VARCHAR(255) NOT NULL,
